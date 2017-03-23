adc619a716e6798a9fad47c1204506c365506de8
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2017-03-23 12:13:15 -0500

    Adding Contact data model

---
+++ ContactList_Challenge/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactList_Challenge.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
